9ef87a1d79cbea3d77f5ac3cb6683617a08c3fa9
Vasilij Melnychuk <isqrel@gmail.com>
2013-01-24 22:16:59 +0200

    Initial

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Enote::Application.config.session_store :cookie_store, key: '_enote_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Enote::Application.config.session_store :active_record_store
